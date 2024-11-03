5ef102f91a7a4e8c8c90caa6bd27a903393f7ac8
AHChaves <arthurhenri2006@gmail.com>
2024-11-03 12:07:41 -0300

    ADD: Q4 TP3

---
+++ C/TP03/Q4.c
@@ -0,0 +1,410 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define TAM_STRING (50 + 1)
+#define TAM_LINHA (1000 + 1)
+#define QTD_POKEMONS 801
+#define TAM_FILA_CIRC 5
+#define TAM_MAX_LISTA 6
+#define NUM_MAX_TYPES 2
+#define NUM_INFO 17
+
+// IMPLEMENTACAO DE LISTA RETIRADA DO GITHUB DA DISCIPLINA
+//--------------------------------------------------------------------------------------------------------------------//
+
+typedef struct L
+{
+    char array[TAM_MAX_LISTA][TAM_STRING]; // Elementos da pilha
+    int n;                                 // Quantidade de array.
+} Lista;
+
+/**
+ * Inicializacoes
+ */
+void start(Lista *l)
+{
+    l->n = 0;
+}
+
+/**
+ * Insere um elemento na primeira posicao da lista e move os demais
+ * elementos para o fim da
+ * @param x int elemento a ser inserido.
+ */
+void inserirInicio(char *x, Lista *l)
+{
+    int i;
+
+    // validar insercao
+    if (l->n >= TAM_MAX_LISTA)
+    {
+        printf("Erro ao inserir!");
+        exit(1);
+    }
+
+    // levar elementos para o fim do array
+    for (i = l->n; i > 0; i--)
+    {
+        strcpy(l->array[i], l->array[i - 1]);
+    }
+
+    strcpy(l->array[0], x);
+    l->n++;
+}
+
+/**
+ * Mostra os array separados por espacos.
+ */
+void mostrar(Lista *l)
+{
+    int i;
+
+    for (i = l->n - 1; i >= 0; i--)
+    {
+        printf("'%s'", l->array[i]);
+        if ((i != 0) && (l->n >= 2))
+            printf(", ");
+    }
+}
+
+// FIM IMPLEMENTACAO LISTA
+//----------------------------------------------------------------------------------------------------------------------------------//
+
+typedef struct Pokemon_s
+{
+    int id;
+    int generation;
+    char name[TAM_STRING];
+    char description[TAM_STRING];
+    Lista *types;
+    Lista *abilities;
+    double weight;
+    double height;
+    int captureRate;
+    bool isLegendary;
+    char captureDate[TAM_STRING];
+} Pokemon;
+
+Pokemon ConstrutorVazio()
+{
+    Pokemon p;
+    p.id = 0;
+    p.generation = 0;
+    strcpy(p.name, "");
+    strcpy(p.description, "");
+    p.types = (Lista *)malloc(sizeof(Lista));
+    p.abilities = (Lista *)malloc(sizeof(Lista));
+    start(p.types);
+    start(p.abilities);
+    p.weight = 0.0;
+    p.height = 0.0;
+    p.captureRate = 0;
+    p.isLegendary = false;
+    strcpy(p.captureDate, "");
+
+    return p;
+}
+
+Pokemon ConstrutorInicializado(int id, int generation, char name[], char description[], double weight, double height, int captureRate, bool isLegendary, char captureDate[])
+{
+    Pokemon p;
+    p.id = id;
+    p.generation = generation;
+    strcpy(p.name, name);
+    strcpy(p.description, description);
+    p.types = (Lista *)malloc(sizeof(Lista));
+    p.abilities = (Lista *)malloc(sizeof(Lista));
+    start(p.types);
+    start(p.abilities);
+    p.weight = weight;
+    p.height = height;
+    p.captureRate = captureRate;
+    p.isLegendary = isLegendary;
+    strcpy(p.captureDate, captureDate);
+
+    return p;
+}
+
+void imprimir(Pokemon poke)
+{
+    printf("[#%d -> %s: %s - [", poke.id, poke.name, poke.description);
+
+    mostrar(poke.types);
+
+    printf("] - [");
+
+    mostrar(poke.abilities);
+
+    printf("] - %0.1lfkg - %0.1lfm - %d%c", poke.weight, poke.height, poke.captureRate, '%');
+    printf(" - %s - %d gen] - %s", (poke.isLegendary) ? "true" : "false", poke.generation, poke.captureDate);
+}
+
+Pokemon Buscar_Pokemon_ID(int id, Pokemon *poke, int n_Poke)
+{
+    Pokemon encontrado = ConstrutorVazio();
+    for (int i = 0; i < n_Poke; i++)
+    {
+        if (id == poke[i].id)
+        {
+            encontrado = poke[i];
+        }
+    }
+    return encontrado;
+}
+
+Pokemon ler(char *linha)
+{
+    Pokemon poke = ConstrutorVazio();
+    char *string = strtok(linha, ",");
+
+    poke.id = atoi(string);
+
+    string = strtok(NULL, ",");
+    poke.generation = atoi(string);
+
+    string = strtok(NULL, ",");
+    strcpy(poke.name, string);
+
+    string = strtok(NULL, ",");
+    strcpy(poke.description, string);
+
+    for (int i = 0; i < NUM_MAX_TYPES; i++)
+    {
+        string = strtok(NULL, ",");
+        if (string != NULL && strcmp(string, "") != 0 && strstr(string, "[") == NULL)
+        {
+            inserirInicio(string, poke.types);
+        }
+    }
+
+    if (strstr(string, "]") != NULL)
+    {
+        char tmp[TAM_STRING];
+        int count = 0;
+        int k = 0;
+        for (int j = 0; j < strlen(string); j++)
+        {
+            if (j != 0)
+            {
+
+                if (string[j] != '[' && string[j] != ']' && string[j] != 39 && string[j] != '"' && count < 2)
+                {
+                    tmp[k] = string[j];
+                    k++;
+                }
+                else if (string[j] == 39 && count <= 2)
+                {
+                    count++;
+                }
+                else if (count >= 2)
+                {
+                    j = strlen(string);
+                }
+            }
+        }
+        tmp[k] = '\0';
+
+        if (string != NULL && strcmp(string, "") != 0)
+        {
+            inserirInicio(tmp, poke.abilities);
+        }
+    }
+    else
+    {
+        int i = 0;
+        while (strstr(string, "]") == NULL)
+        {
+            if (strstr(string, "[") == NULL || poke.abilities->n >= 1)
+            {
+                string = strtok(NULL, ",");
+            }
+
+            char tmp[TAM_STRING];
+            int count = 0;
+            int k = 0;
+            for (int j = 0; j < strlen(string); j++)
+            {
+                if (j != 0)
+                {
+
+                    if (string[j] != '[' && string[j] != ']' && string[j] != 39 && string[j] != '"' && count < 2)
+                    {
+                        tmp[k] = string[j];
+                        k++;
+                    }
+                    else if (string[j] == 39 && count <= 2)
+                    {
+                        count++;
+                    }
+                    else if (count >= 2)
+                    {
+                        j = strlen(string);
+                    }
+                }
+            }
+            tmp[k] = '\0';
+
+            if (string != NULL && strcmp(string, "") != 0)
+            {
+                inserirInicio(tmp, poke.abilities);
+            }
+            i++;
+        }
+    }
+    char array[TAM_MAX_LISTA][TAM_STRING] = {"0", "0", "0", "0", "0", "0"};
+
+    for (int j = 0; j < TAM_MAX_LISTA; j++)
+    {
+        string = strtok(NULL, ",");
+        if (string != NULL)
+        {
+            strcpy(array[j], string);
+        }
+    }
+
+    if (strcmp(array[4], "0") == 0)
+    {
+        poke.weight = atof(array[4]);
+        poke.height = atof(array[3]);
+        poke.captureRate = atoi(array[0]);
+        poke.isLegendary = (strcmp(array[1], "1") == 0) ? true : false; // Lê 1 como true, caso contrário false
+        strcpy(poke.captureDate, array[2]);
+    }
+    else
+    {
+        poke.weight = atof(array[0]);
+        poke.height = atof(array[1]);
+        poke.captureRate = atoi(array[2]);
+        poke.isLegendary = (strcmp(array[3], "1") == 0) ? true : false; // Lê 1 como true, caso contrário false
+        strcpy(poke.captureDate, array[4]);
+    }
+
+    return poke;
+}
+
+//======================================================= Lista de Pokemons =============================================
+
+typedef struct LP
+{
+    Pokemon array[TAM_FILA_CIRC]; // Elementos da pilha
+    int n;                       // Quantidade de array.
+    int inicio;
+    int fim;
+} Fila_Circ_Pokemon;
+
+void start_LP(Fila_Circ_Pokemon *l)
+{
+    l->n = 0;
+    l->fim = 0;
+    l->inicio = 0;
+}
+
+Pokemon remover_FC(Fila_Circ_Pokemon* l)
+{
+    int i;
+    Pokemon resp;
+
+    if (l->n == 0)
+        printf("Erro ao remover!");
+    else
+    {
+        resp = l->array[l->inicio];
+        l->inicio = (l->inicio + 1) % TAM_FILA_CIRC;
+        l->n--;
+    }
+
+    return resp;
+}
+
+void inserir_FC(Pokemon x, Fila_Circ_Pokemon* l)
+{
+    if (l->n >= TAM_FILA_CIRC)
+        remover_FC(l);
+
+    l->array[l->fim] = x;
+    l->fim = (l->fim + 1) % TAM_FILA_CIRC;
+    l->n++;
+
+    int soma = 0;
+    for (int i = 0; i < l->n; i++) {
+        soma += l->array[i].captureRate;
+    }
+    int media = soma / l->n;
+    printf("Média: %d\n", media);
+}
+
+void mostrar_LP(Fila_Circ_Pokemon l)
+{
+    int i;
+
+    for (i = 0; i < l.n; i++)
+    {
+        printf("[%d] ", i);
+        imprimir(l.array[i]);
+    }
+}
+
+//===================================================== Fim Lista de Pokemons =============================================
+
+int main()
+{
+    // FILE *arq = fopen("pokemon.csv", "r");
+    FILE * arq = fopen("/tmp/pokemon.csv", "r");
+
+    char linha[TAM_LINHA];
+    Pokemon *poke = (Pokemon *)malloc(sizeof(Pokemon) * QTD_POKEMONS);
+    Fila_Circ_Pokemon pokeSelect;
+    start_LP(&pokeSelect);
+
+    int poke_index = 0;
+
+    fgets(linha, sizeof(linha), arq); // Descartar a primeira linha
+    while (fgets(linha, sizeof(linha), arq) != NULL)
+    {
+        poke[poke_index] = ler(linha);
+        poke_index++;
+    }
+
+    char entrada[55];
+    strcpy(entrada, "entrada");
+
+    while (strcmp(entrada, "FIM") != 0)
+    {
+
+        scanf("%s", entrada);
+        if (strcmp(entrada, "FIM") != 0)
+        {
+            int id_input = atoi(entrada);
+            inserir_FC(Buscar_Pokemon_ID(id_input, poke, QTD_POKEMONS), &pokeSelect);
+        }
+    }
+
+    int n_operacoes;
+
+    scanf("%d", &n_operacoes);
+
+    for (int i = 0; i < n_operacoes; i++)
+    {
+        char operacao[3];
+        scanf("%2s", operacao);
+        if (strcmp(operacao, "I") == 0)
+        {
+            int id;
+            scanf("%d", &id);
+            inserir_FC(Buscar_Pokemon_ID(id, poke, QTD_POKEMONS), &pokeSelect);
+        }
+        else if (strcmp(operacao, "R") == 0)
+        {
+            Pokemon p = remover_FC(&pokeSelect);
+            printf("(R) %s\n", p.name);
+        }
+    }
+
+    mostrar_LP(pokeSelect);
+
+    free(poke);
+    fclose(arq);
+}
